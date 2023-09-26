f860bf1755d24bfd527cdae3d246e58f39ce725c
ssc0831 <ssc0831@naver.com>
2023-09-26 16:16:22 +0900

    Python BigData

---
+++ Code Test.py
@@ -0,0 +1,12 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=100
+>>> b=50
+>>> result=a+b
+>>> print(a, '+' , b, '=' , result)
+>>> result = a - b
+>>> print(a, '-' , b, '=' , result)
+>>> result = a*b
+>>> print(a, '*' , b, '=' , result)
+>>> result = a/b
+>>> print(a, '/' , b, '=' , result)
